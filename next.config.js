f2eb4c3a98476a15e883be6f1c83fb595a218756
Konstantin Kolesnikov <kosstanley@gmail.com>
2024-07-04 02:19:50 +0500

    Sentry (#3)
    * Added sentry.
    
    * Moved sentry values to env. vars.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
+const { withSentryConfig } = require('@sentry/nextjs')
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   output: 'export',
   images: { unoptimized: true },
 }
 
-module.exports = nextConfig
+// Make sure adding Sentry options is the last code to run before exporting
+module.exports = withSentryConfig(nextConfig, {
+  org: process.env.SENTRY_ORG,
+  project: process.env.SENTRY_PROJECT,
+  authToken: process.env.SENTRY_AUTH_TOKEN,
+  silent: false, // Can be used to suppress logs
+})
