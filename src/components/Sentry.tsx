d7af29ebd942bcc7505a446e76853c852d1ec608
Konstantin Kolesnikov <kosstanley@gmail.com>
2024-09-02 12:13:32 +0200

    Use Sentry tunnel. (#7)

--- src/components/Sentry.tsx
+++ src/components/Sentry.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect } from 'react'
 import * as Sentry from '@sentry/react'
+import { isProduction } from '@/lib/utils'
 
 export const SentryCmp = () => {
   useEffect(() => {
@@ -9,11 +10,16 @@ export const SentryCmp = () => {
       if (process.env.NEXT_PUBLIC_SENTRY_DSN) {
         Sentry.init({
           dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-          integrations: [Sentry.replayIntegration()],
+          integrations: [
+            ...(isProduction()
+              ? [Sentry.replayIntegration({ maskAllText: false })]
+              : []),
+          ],
           tracesSampleRate: 1.0,
           replaysSessionSampleRate: 0.1,
           replaysOnErrorSampleRate: 1.0,
           environment: process.env.NODE_ENV,
+          tunnel: process.env.NEXT_PUBLIC_SENTRY_TUNNEL_URL,
         })
       }
     })
